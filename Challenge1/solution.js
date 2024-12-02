6f095510a90d19f9d7fe8a4e135543013a5e2e39
Ali McNamara <allisonrm95@gmail.com>
2024-12-01 23:36:48 -0500

    optimized and cleaned up

--- Challenge1/solution.js
+++ Challenge1/solution.js
@@ -1,64 +1,41 @@
-// import * as fs from 'fs';
 const fs = require('fs').promises;
 const path = require('path');
-/**
- * read txt file
- * turn into two separate arrays
- * --> Set or Map auxilliary object (one retains original order, one might order them)
- * if just arrays, use .sort() to sort
- *
- * iterate through and store abs val of difference in output array
- * use reduce on output array to find total
- *  */
 
-//Read txt file and
+//read txt file and remove ' ' and '\n'
 const readData = async () => {
   const filePath = path.resolve(__dirname, 'input.txt');
   try {
     const data = await fs.readFile(filePath, 'utf8');
-    const noSpace = data.split(' ').join('');
-    const noNewLine = noSpace.split('\n').join('');
-
-    return noNewLine;
+    const noSpace = data.split(' ').join('').split('\n').join('');
+    return noSpace;
   } catch (error) {
     console.log(`Error in readData: ${error}`);
   }
 };
 
+//sort data into left and right arrays of numbers
 const reshapeData = (data) => {
   let left = true;
   let l = [];
   let r = [];
   let current = '';
-  let counter = 1;
-  for (let i = 0; i < data.length; i++) {
-    current += data[i];
-    if (counter === 5) {
-      console.log('current', current);
-      if (left === true) {
-        l.push(parseInt(current));
-      }
-      if (left === false) {
-        r.push(parseInt(current));
-      }
-      left = !left;
-      counter = 0;
-      current = [];
-    }
-    counter++;
+
+  for (let i = 0; i < data.length; i += 5) {
+    current += data.slice(i, i + 5);
+    left === true ? l.push(parseInt(current)) : r.push(parseInt(current));
+    left = !left;
+    current = '';
   }
   return [l, r];
 };
 
-const orderData = (nestedArr) => {
-  let left = nestedArr[0].sort();
-  let right = nestedArr[1].sort();
-  return [left, right];
+// order left and right arrays
+const orderData = (left, right) => {
+  return [left.sort(), right.sort()];
 };
 
-const findDiff = (nestedArr) => {
-  let left = nestedArr[0];
-  let right = nestedArr[1];
+// find difference between coordinating indices of left and right arrays
+const findDiff = (left, right) => {
   let differences = [];
   for (let i = 0; i < left.length; i++) {
     differences.push(Math.abs(left[i] - right[i]));
@@ -66,20 +43,21 @@ const findDiff = (nestedArr) => {
   return differences;
 };
 
+// find sum of differences
 const findSum = (diffArr) => {
   const diffSum = diffArr.reduce((acc, curr) => {
     return acc + curr;
   }, 0);
-  console.log('diffSum', diffSum);
   return diffSum;
 };
 
+// chain each step
 const main = async () => {
   try {
     const data = await readData();
     const leftAndRight = await reshapeData(data);
-    const orderedData = orderData(leftAndRight);
-    const differences = findDiff(orderedData);
+    const orderedData = orderData(...leftAndRight);
+    const differences = findDiff(...orderedData);
     const diffSum = findSum(differences);
     return diffSum;
   } catch (error) {
